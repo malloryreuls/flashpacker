34a3300235e839542dc7203e90d600a6c9459663
Mallory Reulman <mreulman@gmail.com>
2014-07-25 19:21:05 -0700

    creates sign in and sign up views

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,5 @@
 class Spot < ActiveRecord::Base
+
+	belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+
+  	has_secure_password
+
+	def password
+  		@password
+ 	end
+
+	def password=(new_password)
+	  @password = new_password
+	  self.password_digest = BCrypt::Password.create(new_password)
+	end
+
+  	def authenticate(test_password)
+  		if BCrypt::Password.new(self.password_digest) == test_password
+  			self
+  		else
+  			false
+  		end
+  	end
+
+  	has_many :spots
+
 end
